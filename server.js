523de40c684a209aff898487f93c3d955093dbb7
xiaodongliang <xiaodongliang@hotmail.com>
2023-11-03 22:09:20 +0800

    init v

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { PORT } = require('./config.js');
+
+let app = express();
+app.use(express.static('wwwroot'));
+app.use(require('./routes/auth.js'));
+app.use(require('./routes/models.js'));
+app.use(require('./routes/proxy.js'));
+
+app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`); });
